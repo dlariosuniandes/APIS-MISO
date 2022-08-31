4fb9bdcdd5c1a0363df8186bb2cd2becedddf473
Daniel Larios <daniel.larios.torres@nui.care>
2022-08-31 22:28:48 +0200

    Se agrega configuración de TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,39 @@ import { ProductModule } from './product/product.module';
 import { CountryModule } from './country/country.module';
 import { RecipeModule } from './recipe/recipe.module';
 import { RestaurantModule } from './restaurant/restaurant.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CultureEntity } from './culture/culture.entity';
+import { RestaurantEntity } from './restaurant/restaurant.entity';
+import { RecipeEntity } from './recipe/recipe.entity';
+import { CountryEntity } from './country/country.entity';
+import { ProductEntity } from './product/product.entity';
 
 @Module({
-  imports: [CultureModule, ProductModule, CountryModule, RecipeModule, RestaurantModule],
+  imports: [
+    CultureModule,
+    ProductModule,
+    CountryModule,
+    RecipeModule,
+    RestaurantModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'culture',
+      entities: [
+        CultureEntity,
+        RestaurantEntity,
+        RecipeEntity,
+        CountryEntity,
+        ProductEntity,
+      ],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
