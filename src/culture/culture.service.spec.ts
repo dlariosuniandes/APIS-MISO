0ad095c494b49b1b774c62bd3e6ca4f97e8cb088
Daniel Larios <daniel.larios.torres@nui.care>
2022-09-01 23:13:31 +0200

    Se añade pruebas restantes de culture

--- src/culture/culture.service.spec.ts
+++ src/culture/culture.service.spec.ts
@@ -50,4 +50,93 @@ describe('CultureService', () => {
     expect(cultures).not.toBeNull();
     expect(cultures).toHaveLength(cultureList.length);
   });
+
+  it('findOne should return a culture by id', async () => {
+    const storedCulture: CultureEntity = cultureList[0];
+    const culture: CultureEntity = await service.findOne(storedCulture.id);
+    expect(culture).not.toBeNull();
+    expect(culture.name).toEqual(storedCulture.name);
+    expect(culture.description).toEqual(storedCulture.description);
+    expect(culture.countries).toEqual(storedCulture.countries);
+    expect(culture.recipes).toEqual(storedCulture.recipes);
+    expect(culture.products).toEqual(storedCulture.products);
+    expect(culture.restaurants).toEqual(storedCulture.restaurants);
+  });
+
+  it('findOne should throw an exception for an invalid culture', async () => {
+    await expect(() => service.findOne('0')).rejects.toHaveProperty(
+      'message',
+      'The culture with the given id was not found',
+    );
+  });
+
+  it('create should return a new culture', async () => {
+    const culture: CultureEntity = {
+      id: faker.datatype.uuid(),
+      name: faker.lorem.sentence(),
+      description: faker.lorem.sentence(),
+      countries: [],
+      recipes: [],
+      products: [],
+      restaurants: [],
+    };
+
+    const newCulture: CultureEntity = await service.create(culture);
+    expect(newCulture).not.toBeNull();
+
+    const storedCulture: CultureEntity = await repository.findOne({
+      where: { id: newCulture.id },
+    });
+    expect(storedCulture).not.toBeNull();
+    expect(storedCulture.name).toEqual(newCulture.name);
+    expect(storedCulture.description).toEqual(newCulture.description);
+  });
+
+  it('update should modify a culture', async () => {
+    const culture: CultureEntity = cultureList[0];
+    culture.name = 'New name';
+    culture.description = 'New description';
+    const updatedCulture: CultureEntity = await service.update(
+      culture.id,
+      culture,
+    );
+    expect(updatedCulture).not.toBeNull();
+    const storedCulture: CultureEntity = await repository.findOne({
+      where: { id: culture.id },
+    });
+    expect(storedCulture).not.toBeNull();
+    expect(storedCulture.name).toEqual(culture.name);
+    expect(storedCulture.description).toEqual(culture.description);
+  });
+
+  it('update should throw an exception for an invalid culture', async () => {
+    let culture: CultureEntity = cultureList[0];
+    culture = {
+      ...culture,
+      name: 'New name',
+      description: 'New description',
+    };
+    await expect(() => service.update('0', culture)).rejects.toHaveProperty(
+      'message',
+      'The culture with the given id was not found',
+    );
+  });
+
+  it('delete should remove a culture', async () => {
+    const culture: CultureEntity = cultureList[0];
+    await service.delete(culture.id);
+    const deletedCulture: CultureEntity = await repository.findOne({
+      where: { id: culture.id },
+    });
+    expect(deletedCulture).toBeNull();
+  });
+
+  it('delete should throw an exception for an invalid culture', async () => {
+    const culture: CultureEntity = cultureList[0];
+    await service.delete(culture.id);
+    await expect(() => service.delete('0')).rejects.toHaveProperty(
+      'message',
+      'The culture with the given id was not found',
+    );
+  });
 });
