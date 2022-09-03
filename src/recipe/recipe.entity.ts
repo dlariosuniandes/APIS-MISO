7619d7d1b56fdad603f6472d1d4937711b14cc0e
Juan Carlos Nieto Z <jc.nieto@uniandes.edu.co>
2022-09-03 10:13:56 -0500

    Se adiciona la entidad recipe

--- src/recipe/recipe.entity.ts
+++ src/recipe/recipe.entity.ts
@@ -1,10 +1,25 @@
 import { CultureEntity } from 'src/culture/culture.entity';
-import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class RecipeEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  preparation: string;
+
+  @Column()
+  url_photo: string;
+
+  @Column()
+  url_video: string;
 
   @ManyToOne(() => RecipeEntity, (recipe) => recipe.culture)
   culture: CultureEntity;
