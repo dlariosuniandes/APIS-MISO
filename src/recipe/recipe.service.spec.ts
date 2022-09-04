1c959a7f48d02a6873a178da63de9c53b1f4ce35
Juan Carlos Nieto Z <jc.nieto@uniandes.edu.co>
2022-09-03 20:32:56 -0500

    Service para Recipe

--- src/recipe/recipe.service.spec.ts
+++ src/recipe/recipe.service.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { TypeOrmTestingConfig } from 'src/shared/testing-utils/typeorm-testing-config';
@@ -8,6 +9,27 @@ import { RecipeService } from './recipe.service';
 describe('RecipeService', () => {
   let service: RecipeService;
   let repository: Repository<RecipeEntity>;
+  let recipeList: RecipeEntity[];
+
+  const generateRecipe = () => {
+    const recipeDict: object = {
+      id: faker.datatype.uuid(),
+      name: faker.lorem.sentence(),
+      preparation: faker.lorem.sentence(),
+      url_photo: faker.image(),
+      url_video: faker.internet(),
+    };
+    return recipeDict;
+  };
+
+  const seedDatabase = async () => {
+    await repository.clear();
+    recipeList = [];
+    for (let i = 0; i < 5; i++) {
+      const recipe: RecipeEntity = await repository.save(generateRecipe());
+      recipeList.push(recipe);
+    }
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -23,5 +45,52 @@ describe('RecipeService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a new recipe', async () => {
+    const newRecipe = Object.assign(new RecipeEntity(), generateRecipe());
+    await service.create(newRecipe);
+    const newStoredRecipe = await service.findOne(newRecipe.id);
+    expect(newStoredRecipe).not.toBeNull();
+    const newStoredRecipeObj = JSON.parse(JSON.stringify(newStoredRecipe));
+    delete newStoredRecipeObj['cultures'];
+    expect(JSON.parse(JSON.stringify(newRecipe))).toEqual(newStoredRecipeObj);
+  });
+  
+  it('existing recipes should be found', async () => {
+    const storedRecipe: RecipeEntity = recipeList[recipeList.length - 1];
+    const recipe: RecipeEntity = await service.findOne(storedRecipe.id);
+    expect(recipe).not.toBeNull();
+    const recipeObj = JSON.parse(JSON.stringify(recipe));
+    delete recipeObj['cultures'];
+    expect(recipeObj).toEqual(JSON.parse(JSON.stringify(storedRecipe)));
+  });
+
+  it('should edit an existing recipe', async () => {
+    const editingRecipe = recipeList[recipeList.length - 1];
+    editingRecipe.preparation = faker.lorem.sentence();
+    let storedRecipe: RecipeEntity = await service.findOne(
+      editingRecipe.id,
+    );
+    const { cultures, ...rest } = JSON.parse(JSON.stringify(storedRecipe));
+    expect(storedRecipe).not.toBeNull();
+    expect(JSON.parse(JSON.stringify(editingRecipe))).not.toBe(rest);
+    await service.updateOne(editingRecipe.id, editingRecipe);
+    storedRecipe = await service.findOne(editingRecipe.id);
+    const storedRecipeObj = JSON.parse(JSON.stringify(storedRecipe));
+    delete storedRecipeObj['cultures'];
+    expect(JSON.parse(JSON.stringify(editingRecipe))).toEqual(
+      storedRecipeObj,
+    );
+  });
+
+  it('should delete an existing recipe', async () => {
+    await service.deleteOne(recipeList[0].id);
+    const recipes: RecipeEntity[] = await service.findAll();
+    expect(recipes.length).toEqual(4);
+  });
+
+  afterAll(() => {
+    repository.clear();
   });
 });
--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from 'src/shared/errors';
 import { Repository } from 'typeorm';
 import { RecipeEntity } from './recipe.entity';
 
@@ -7,6 +8,55 @@ import { RecipeEntity } from './recipe.entity';
 export class RecipeService {
     constructor(
         @InjectRepository(RecipeEntity)
-        private readonly cultureRepository: Repository<RecipeEntity>,
-    ){}
+        private readonly recipeRepository: Repository<RecipeEntity>,
+    ) {}
+
+    async create(recipe: RecipeEntity): Promise<RecipeEntity> {
+        return await this.recipeRepository.save(recipe);
+    }
+
+    async findAll(): Promise<RecipeEntity[]>{
+        return await this.recipeRepository.find();
+    }
+
+    async findOne(id: string): Promise<RecipeEntity> {
+        const recipe: RecipeEntity = await this.recipeRepository.findOne({
+            where: { id },
+            relations: ['cultures'],
+        });
+        if (!recipe) {
+            throw new NotFoundException(
+                'The culture with the given id was not found',
+            );
+        }
+        return recipe;
+    }
+
+    async updateOne(id: string, recipe:RecipeEntity): Promise<RecipeEntity> {
+        const recipeDB: RecipeEntity = await this.recipeRepository.findOne({
+            where: { id },
+            relations: ['cultures'],
+        });
+        if (!recipeDB) {
+            throw new NotFoundException(
+                'The culture with the given id was not found',
+            );
+        }
+        return await this.recipeRepository.save({ ...recipeDB, ...recipe });
+    }
+
+    async deleteOne(id: string): Promise<string> {
+        const recipeDB: RecipeEntity = await this.recipeRepository.findOne({
+            where: { id },
+            relations: ['cultures'],
+        });
+        if (!recipeDB) {
+            throw new BusinessLogicException(
+                "The culture with the given id was not found",
+                BusinessError.NOT_FOUND
+            );
+        }
+        await this.recipeRepository.remove(recipeDB);
+        return `recipe ${id} was successfully removed`;
+    }
 }
