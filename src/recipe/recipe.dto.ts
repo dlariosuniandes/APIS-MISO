b692d47e3710fb3cd8bce522fd588eb195187fcf
jc-nieto <jc.nieto@uniandes.edu.co>
2022-10-02 11:33:59 -0500

    graphQL recipe

--- src/recipe/recipe.dto.ts
+++ src/recipe/recipe.dto.ts
@@ -1,22 +1,29 @@
+import { Field, InputType } from '@nestjs/graphql';
 import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
 
+@InputType()
 export class RecipeDto {
+  @Field()
   @IsString()
   @IsNotEmpty()
   readonly name: string;
 
+  @Field()
   @IsString()
   @IsNotEmpty()
   description: string;
 
+  @Field()
   @IsString()
   @IsNotEmpty()
   preparation: string;
 
+  @Field()
   @IsUrl()
   @IsNotEmpty()
   url_photo: string;
 
+  @Field()
   @IsUrl()
   @IsNotEmpty()
   url_video: string;
